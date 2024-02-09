d4f5a431679e1a678c23673b9ec789cd58632e56
dtsivkovski <dtsivkovs@gmail.com>
2024-02-08 20:17:08 -0800

    added button

--- fantastic-engine/src/app/page.js
+++ fantastic-engine/src/app/page.js
@@ -1,5 +1,15 @@
 import Image from "next/image";
 
+// create next.js component for a button with a custom title and href
+const ButtonComponent = ({ title = "Go to About", href = "/about" }) => {
+  return (
+    <a href={href} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+      {title}
+    </a>
+  );
+};
+
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
@@ -11,6 +21,7 @@ export default function Home() {
         <p className="text-xl text-center m-6">
           Testing out Next.js and Tailwind CSS
         </p>
+        <ButtonComponent title="Click me!" href="/about" />
       </div>
 
     </main>
